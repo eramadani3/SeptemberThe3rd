d5ddefad005d2e01349ba0158ea411e41b9242c8
Engjell Ramadani <75699521+eramadani3@users.noreply.github.com>
2022-03-30 16:32:37 -0400

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,69 @@
+import { Facebook, Instagram, Twitter } from "@material-ui/icons";
+import styled from "styled-components"
+
+const Container = styled.div`
+    display: flex;
+`;
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+`;
+
+const Logo = styled.h1`
+
+`;
+const Desc = styled.p`
+    margin: 20px 0px;
+`;
+const SocialContainer = styled.div`
+    display: flex;
+`;
+const SocialIcon = styled.div`
+    widthL 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: white;
+    background-color: ${props=>props.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
+`;
+
+const Center = styled.div`
+    flex: 1;
+    padding: 20px;
+`;
+const Right = styled.div`
+    flex: 1;
+    padding: 20px;
+`;
+
+
+
+const Footer = () => {
+    return (
+        <Container>
+            <Left>
+                <Logo>September The 3rd</Logo>
+                <SocialContainer>
+                    <SocialIcon color="black">
+                        <Facebook/>
+                    </SocialIcon>
+                    <SocialIcon color="black">
+                        <Instagram/>
+                    </SocialIcon>
+                    <SocialIcon color="black">
+                        <Twitter/>
+                    </SocialIcon>
+                </SocialContainer>
+            </Left>
+            <Center></Center>
+            <Right></Right>
+        </Container>
+    )
+}
+
+export default Footer
---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://i.pinimg.com/736x/d2/fb/68/d2fb68dd6c1d65aeee2a398a658082f5.jpg",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "F5FAFD",
+    },
+    {
+      id: 2,
+      img: "https://yt3.ggpht.com/G4iqAXZOiT3i6bd-Yun9sU-mNh_f3VUhJ7fVU_vZzk9VHpiCviH2PcoNhsOdc6q7pcSjw4D-eA=s900-c-k-c0x00ffffff-no-rj",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "FCF1ED",
+    },
+    {
+      id: 3,
+      img: "https://i.pinimg.com/564x/7c/9a/16/7c9a162e8a3a5761dc343e3301869017.jpg",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "FBF0F4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    },
+  ]
