062925927901256acb1b7ff7a5c0d496d9af2983
Engjell <eramadani@wpi.edu>
2022-06-09 13:06:34 -0400

    finished product page

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -19,9 +19,13 @@ const Filter = styled.div`
 const FilterText = styled.span`
     font-size: 20px;
     font-weight: 600;
+    margin-right: 20px;
 `;
 
-const Select = styled.select``
+const Select = styled.select`
+    padding: 10px;
+    margin-right:20px;
+`;
 const Option = styled.option``
 const ProductList = () => {
   return (
@@ -41,9 +45,22 @@ const ProductList = () => {
                     <Option>Yellow</Option>
                     <Option>Green</Option>
                 </Select>
+                <Select>
+                    <Option disabled selected>Size</Option>
+                    <Option>XS</Option>
+                    <Option>S</Option>
+                    <Option>M</Option>
+                    <Option>L</Option>
+                    <Option>XL</Option>
+                </Select>
                 </Filter>
             <Filter>
-                <FilterText>Sort Products</FilterText>
+                <FilterText>Sort Products: </FilterText>
+                <Select>
+                    <Option selected>Newest</Option>
+                    <Option>Price (asc)</Option>
+                    <Option>Price (desc)</Option>
+                </Select>
                 </Filter>
         </FilterContainer>
         <Products/>
